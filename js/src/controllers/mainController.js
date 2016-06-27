a1142cda419bc5ab30523c261163430f9e0e7887
Dominik Janik <mortyymer@gmail.com>
2016-06-27 20:51:45 +0200

    Added statuses

--- js/src/controllers/mainController.js
+++ js/src/controllers/mainController.js
@@ -4,4 +4,15 @@ app.controller("MainController",function($scope, $http){
        .then(function(res){
           $scope.HostsList = res.data;                
     });
+       $scope.GetStatus=function(_status){
+       		if(_status == 1){
+       			return "Online";
+       		}
+       		else if(_status == 0){
+       			return "Offline";
+       		}
+       }
+       $scope.GoToDetails=function(host.IP){
+       		
+       }
 })
