b18bda58e6c73d7bfe847ae3fa530558866fdeea
Dominik Janik <mortyymer@gmail.com>
2016-06-27 21:49:41 +0200

    Some tweaks in states, added rest of json files, improvements in controller

--- js/src/app.js
+++ js/src/app.js
@@ -2,16 +2,22 @@ var app = angular.module('App', ['ui.router']);
 app.config(function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/Home");
+  $urlRouterProvider.otherwise("/");
   //
   // Now set up the states
   	$stateProvider
-    	.state('Home', {
-      	url: "/Home",
-      	templateUrl: "sites/home.html"
+    .state('/', {
+    		url:"/",
+      		templateUrl: "sites/home.html",
+      		controller: 'MainController'
     })
     .state('List', {
-      url: "/List",
-      templateUrl: "sites/list.html",
-      controller: 'MainController'
-});})
+		templateUrl: "sites/list.html",
+	   	controller: 'MainController'
+	})
+	.state('Details', {
+		templateUrl: "sites/details.html",
+	   	controller: 'MainController',
+	   	params : { host: null }
+	});
+})
--- js/src/controllers/mainController.js
+++ js/src/controllers/mainController.js
@@ -1,18 +1,45 @@
-app.controller("MainController",function($scope, $http){
+app.controller("MainController",function($scope, $http, $state, $stateParams){
+
+    $scope.History = [];
 	$scope.HostsList = [];
-	$http.get('files/addresses.json')
-       .then(function(res){
-          $scope.HostsList = res.data;                
-    });
-       $scope.GetStatus=function(_status){
-       		if(_status == 1){
-       			return "Online";
-       		}
-       		else if(_status == 0){
-       			return "Offline";
-       		}
-       }
-       $scope.GoToDetails=function(host.IP){
-       		
-       }
+	$scope.online = 0;
+	$scope.offline = 0;
+	
+
+    $scope.GetStatus=function(_status){
+    	if(_status == 1){
+    		return "Online";
+   		}
+    	else if(_status == 0){
+    		return "Offline";
+    	}
+    }
+    $scope.GetHistory = function(address){
+       	$http.get('files/' + address)
+       		.then(function(res){
+          		$scope.History = res.data;                
+	    	});
+    }
+    $scope.LoadStatuses = function(){
+    	$http.get('files/addresses.json')
+    	    .then(function(res){
+          		$scope.HostsList = res.data;                
+          		for(var i=0;i<$scope.HostsList.length;i++){
+          		if($scope.HostsList[i].Status == 1){
+          			$scope.online++;
+          		}
+          		else{
+	          		$scope.offline++;	
+          		}
+          	}
+    	});
+    }
+    $scope.init = function () {
+    if ($stateParams != null && $stateParams.host !=null) {
+        	$scope.selectedHost = $stateParams.host;
+			$scope.GetHistory($scope.selectedHost.LogPath);
+    } 
+    $scope.LoadStatuses();
+	}
+	$scope.init();
 })
